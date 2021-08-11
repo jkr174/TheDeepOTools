dd2cf668b6a33214fa1729ad994eb22b99b06f61
jkr174 <romo17410@gmail.com>
2021-08-10 20:54:12 -0500

    Update
    To do: need to play around with maintenance and play around with it.

---
+++ UserManagement.MVC/Models/Maintaince.cs
@@ -0,0 +1,27 @@
+﻿/* Name:    Jovany Romo
+ * Date:    6/3/2021
+ * Summary: Inventory Model class for the data of an inventory item. 
+ */
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheDeepOTools.Models
+{
+    public class Maintaince
+    {
+        [Key]
+        public int ItemID { get; set; }
+        [Required(ErrorMessage = "Please enter an Item Identifier")]
+        public string ItemIdentifier { get; set; }
+        [Required(ErrorMessage = "Please enter a product name")]
+        public string ItemName { get; set; }
+        [Required(ErrorMessage = "Please enter a description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Please specify a category")]
+        public string Category { get; set; }
+        public string Subcategory { get; set; }
+        public DateTime TotalHrs { get; set; }
+
+    }
+}
